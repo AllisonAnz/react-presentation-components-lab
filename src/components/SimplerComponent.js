a306d2bf1cc50eb90dc222bbdf25a245cbd35687
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-21 16:02:03 -0500

    commit tree 031729fa9dff1c98be21eefaaff8adb00e3e83f1

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,9 @@
 // Code SimplerComponent Here
+//Specs: create a SimplerComp. 
+// Should be "stateless functional"
+// Should render a <div> to the page that contains the text "I'm just happy"
+import React from 'react'
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent

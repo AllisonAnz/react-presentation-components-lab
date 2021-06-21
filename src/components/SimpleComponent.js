e6d88d11cc663b9a853cc30158c15c80d08e8508
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-21 16:02:22 -0500

    Completed Assignment

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponent Here
+//specs: Create a SimpleComponent
+// Should be decared as a class (instance of Comp.) so that it can carry state 
+// Should have a state property called mood that default value of happy
+// Should simply render its current mood state to the page in a div 
+// Should implement a handleClick function that can serve as a cb to the <div>'s click event 
+// When clicked, the comps mood should toggle between happy and sad states 
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mood: 'happy'
+        };
+    }
+
+    handleClick = () => {
+        const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+        this.setState({ mood: newMood });
+    }
+
+    render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>;
+    }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
-
+//currently the SimplerComp. receives one prop called handleClick
+//which is currently undefined. 
+//Rewrite this prop to perform an action
+//Then, make sure SimplerComponent can trigger this action when clicked on
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("You clicked me!")} />
   </div>,
   document.getElementById('root')
 );
